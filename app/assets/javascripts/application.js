063167a4d8cc071c620e7dd19bf148b18b85a0a3
Rory Trunkhill <rtrunkhill@gmail.com>
2017-10-31 20:45:19 +0000

    i guess this works?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+var blocmetrics = {};
+blocmetrics.report = function(eventName){
+   var event = {event: { name: eventName }};
+   var request = new XMLHttpRequest();
+   request.open("POST", "http://blocmetrics-rtrunkhill.c9users.io/api/events", true);
+   request.setRequestHeader('Content-Type', 'application/json');
+   request.send(JSON.stringify(event));
+};
